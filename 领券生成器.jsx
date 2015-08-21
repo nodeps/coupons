654405a5b3f6164914bc9840f4a9b3033620f1e3
bruce.wang <37991842@qq.com>
2015-08-21 19:38:54 +0800

    Update 领券生成器.jsx

--- 领券生成器.jsx
+++ 领券生成器.jsx
@@ -1,103 +1,53 @@
-/**
-* @@@BUILDINFO@@@ 根据参考图层组命名其它图层组.jsx !Version! Fri Apr 19 2013 17:46:01 GMT+0800
-*kingmax_res@163.com
+
+// Written by 王小洲 37991842
+// create date:2015-08-12
+
+/*
+领券生成器.jsx 1.0
 */
+
 #target photoshop
-app.bringToFront();
-function SaveLayerNameFromLayerSet(ref)
-{
-        var names = new Array();
-        var cLayerSet = ref.activeLayer;        
-        
-        if(cLayerSet.typename == 'LayerSet')
-        {
-                for(var i = 0; i<cLayerSet.layers.length; i++)
-                {
-                        if(cLayerSet.layers[i].typename == 'ArtLayer')
-                        {
-                                 names.push(cLayerSet.layers[i].name);
-                        }
-                }
-        }
-    
-        return names;
+
+app.preferences.rulerUnits = Units.PIXELS; // set Pixel for checking bounds
+var doc = app.activeDocument;
+var buttonB,coupon,button,buttonB,rule,foot;
+var names = new Array();
+function getLayerY (name) {
+	var currentLayer = doc.layers[i].merge();
+	var Ypoint = currentLayer.bounds[1];
+	return Ypoint;
+	
 }
-function RenameLayerSet(selectedLayerSet, namesArray)
-{
-        if(selectedLayerSet.typename == 'LayerSet' && selectedLayerSet.layers.length == namesArray.length)
-        {
-                for(var i = 0; i<selectedLayerSet.layers.length; i++)
-                {
-                    selectedLayerSet.layers[i].name = namesArray[i];
-                }
-        }
-        else
-        {
-                alert('图层数量不一致!', selectedLayerSet.name);
-        }
+for(var i =0; i<doc.layers.length; i++){
+	
+	if(doc.layers[i].visible==false){
+		//第一级图层为不可见
+	}else if(doc.layers[i].typename == 'LayerSet' ){
+		
+		if (doc.layers[i].name == 'coupon'){
+			coupon = getLayerY('coupon');
+		}
+		if (doc.layers[i].name == 'button'){
+			var currentLayer = doc.layers[i].merge();
+			button = currentLayer.bounds[1];
+			buttonB = currentLayer.bounds[3];
+		}
+		if (doc.layers[i].name == 'rule'){
+			rule = getLayerY('rule');
+		}
+		if (doc.layers[i].name == 'foot'){
+			foot = getLayerY('foot');
+		}
+		
+	}else if(doc.layers[i].name == 'rule-txt'){
+		var ruleTxt = doc.layers[i];
+		var rr =ruleTxt.textItem.contents;
+		
+	}
 }
-function Main()
-{
-        var win = new Window('dialog','根据参考图层组命名其它图层组');
-        win.size = [200,120];
-        
-         var names = new Array();
-         var refLayerSet;
-        
-        //var panel = win.add('panel',undefined,'test');
-        var btSave = win.add('button', undefined, '存储当前图层组中层名');
-        //btSave.enabled = false;
-        var btRename = win.add('button', undefined, '---重命名选择图层组---');
-        btRename.enabled = false;
-        
-        var cbAllLayerSet = win.add('checkBox', undefined, '对所有图层组有效!');
-        cbAllLayerSet.value = true;
-        /*
-        btSave.enabled = function()
-        {
-                return activeDocument.activeLayer.typename == 'LayerSet';
-        }
-        */
-    
-        btSave.onClick = function()
-        {
-                    names = SaveLayerNameFromLayerSet (activeDocument);
-                    
-                    if(names.length != 0)
-                    {
-                            btRename.enabled = true;
-                            refLayerSet = activeDocument.activeLayer;
-                            alert (names, '保存完毕');
-                    }
-                    else
-                    {
-                            alert('当前选择可能不是图层组!','错误');
-                    }
-        }
-    
-        btRename.onClick = function()
-        {
-                //alert(cbAllLayerSet.value);
-                var doc = activeDocument;
-                if(cbAllLayerSet.value)
-                {
-                        for(var i =0; i<doc.layers.length; i++)
-                        {
-                                if(doc.layers[i].typename == 'LayerSet' && doc.layers[i] != refLayerSet)
-                                {
-                                    RenameLayerSet(doc.layers[i], names);
-                                }
-                        }
-                }
-                else
-                {
-                        RenameLayerSet(doc.activeLayer, names);
-                }
-        }
-        
-        win.show();
-}
-if(documents.length > 0)
-{
-        Main();
-}
+
+names =[coupon/2,coupon,button,buttonB,rule,foot];
+alert(names);
+doc.close(SaveOptions.DONOTSAVECHANGES);
+
+
